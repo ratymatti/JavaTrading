13b74dc766e73e5f0b57a8ed5d506a1ba32d546f
Matti Räty <mattiraty@hotmail.com>
2024-01-03 11:43:48 +0200

    created MarginAccount class

--- pojo/MarginAccount.java
+++ pojo/MarginAccount.java
@@ -1,6 +1,34 @@
 package pojo;
 
-public class MarginAccount  {
+import java.math.BigDecimal;
+
+public class MarginAccount extends TradeAccount {
+    private BigDecimal margin;
+
+    public MarginAccount(BigDecimal margin, String id) {
+        super(id);
+        setMargin(margin);
+    }
+
+    public MarginAccount(MarginAccount source) {
+        super(source.getId());
+        setMargin(source.getMargin());
+    }
 
 
+    public BigDecimal getMargin() {
+        return this.margin;
+    }
+
+    public void setMargin(BigDecimal margin) {
+        if (margin == null || margin.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("Cash balance must be a non-null positive value");
+        }
+        this.margin = margin;
+    }
+
+    @Override
+    public TradeAccount clone() {
+        return new MarginAccount(this);
+    }
 }
