ba48735da16c43b2045fb46c654c2e77de8ea826
Matti Räty <mattiraty@hotmail.com>
2024-01-03 12:00:42 +0200

    created TradeAccountRepository class

--- repository/TradeAccountRepository.java
+++ repository/TradeAccountRepository.java
@@ -1,7 +1,27 @@
 package repository;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import pojo.TradeAccount;
 
 public class TradeAccountRepository {
+    private Map<String, TradeAccount> datastore = new HashMap<>();
 
+    public void createTradeAccount(TradeAccount tradeAccount) {
+        this.datastore.put(tradeAccount.getId(), tradeAccount.clone());
+    }
+
+    public TradeAccount retrieveTradeAccount(String id) {
+        return datastore.get(id).clone();
+    }
+
+    public void updateTradeAccount(TradeAccount tradeAccount) {
+        if (!datastore.containsKey(tradeAccount.getId())) {
+            throw new IllegalArgumentException("TradeAccount with ID " + tradeAccount.getId() + " does not exist. Please use createTradeAccount()");
+        }
+        this.datastore.replace(tradeAccount.getId(), tradeAccount.clone());
+    }
+    
 
 }
