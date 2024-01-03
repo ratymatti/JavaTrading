f15f1eb04c1e48ee77abdca2914d68e7e5b71ab3
Matti Räty <mattiraty@hotmail.com>
2024-01-03 11:38:19 +0200

    created CashAccount class

--- pojo/CashAccount.java
+++ pojo/CashAccount.java
@@ -1,6 +1,36 @@
 package pojo;
 
-public class CashAccount {
+import java.math.BigDecimal;
+
+public class CashAccount extends TradeAccount {
+        private BigDecimal cashBalance;
 
 
+    public CashAccount(BigDecimal cashBalance, String id) {
+        super(id);
+        setCashBalance(cashBalance);
+    }
+
+    public CashAccount(CashAccount source) {
+        super(source.getId());
+        setCashBalance(source.getCashBalance());
+    }
+
+
+    public BigDecimal getCashBalance() {
+        return this.cashBalance;
+    }
+
+    public void setCashBalance(BigDecimal cashBalance) {
+        if (cashBalance == null || cashBalance.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("Cash balance must be a non-null positive value");
+        }
+        this.cashBalance = cashBalance;
+    }
+
+
+    @Override
+    public TradeAccount clone() {
+        return new CashAccount(this);
+    }
 }
