85b335c92f5bf171bb85f81fb2832750fbf89a8a
Matti Räty <mattiraty@hotmail.com>
2024-01-04 12:30:18 +0200

    working on CashAccountService class

--- service/CashAccountService.java
+++ service/CashAccountService.java
@@ -1,7 +1,31 @@
 package service;
 
+import java.math.BigDecimal;
 
-public class CashAccountService {
+import pojo.CashAccount;
+import repository.TradeAccountRepository;
 
+public class CashAccountService implements TradeAccountService {
 
+    private TradeAccountRepository repository;
+
+    @Override
+    public void deposit(String id, BigDecimal amount) {
+        CashAccount account = retrieveTradeAccount(id);
+        account.setCashBalance(account.getCashBalance().add(amount));
+        updateTradeAccount(account);    
+    }
+
+    @Override
+    public void withdraw(String id, BigDecimal amount) {
+        
+    }
+
+    public CashAccount retrieveTradeAccount(String id) {
+        return (CashAccount) this.repository.retrieveTradeAccount(id); 
+    }
+
+    public void updateTradeAccount(CashAccount tradeAccount) {
+        this.repository.updateTradeAccount(tradeAccount);
+    }
 }
