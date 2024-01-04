f315f4bf1c1b4f39eb64aac3a21defe233268cd7
Matti Räty <mattiraty@hotmail.com>
2024-01-04 12:42:46 +0200

    created MarginAccountService class

--- service/MarginAccountService.java
+++ service/MarginAccountService.java
@@ -1,7 +1,42 @@
 package service;
 
+import java.math.BigDecimal;
 
-public class MarginAccountService {
+import pojo.MarginAccount;
+import repository.TradeAccountRepository;
 
+public class MarginAccountService implements TradeAccountService {
+
+    private TradeAccountRepository repository;
+
+    @Override
+    public void deposit(String id, BigDecimal amount) {
+        MarginAccount account = retrieveTradeAccount(id);    
+        account.setMargin(account.getMargin().add(amount));
+        updateTradeAccount(account);
+    }
+
+    @Override
+    public void withdraw(String id, BigDecimal amount) {
+        MarginAccount account = retrieveTradeAccount(id);   
+        account.setMargin(account.getMargin().subtract(amount));
+        updateTradeAccount(account);
+    }
+
+    public MarginAccount retrieveTradeAccount(String id) {
+        return (MarginAccount) this.repository.retrieveTradeAccount(id); 
+    }
+
+    public void updateTradeAccount(MarginAccount tradeAccount) {
+        this.repository.updateTradeAccount(tradeAccount);
+    }
+
+    public void deleteTradeAccount(String id) {
+        this.repository.deleteTradeAccount(id);
+    }
+
+    public void createTradeAccount(MarginAccount tradeAccount) {
+        this.repository.createTradeAccount(tradeAccount);
+    }
 
 }
